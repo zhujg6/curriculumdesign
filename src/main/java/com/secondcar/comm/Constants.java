a0d5be0eb65a1c64d0c7f3af6751f8bf7e324603
zhujg <zhujg@asiainfo.com>
2016-11-23 16:20:47 +0800

    id=常量

---
+++ src/main/java/com/secondcar/comm/Constants.java
@@ -0,0 +1,12 @@
+package com.secondcar.comm;
+
+public class Constants {
+	//用户权限
+	//普通用户
+	public static final Integer PERMISSION_USER = 1; 
+	//会员
+	public static final Integer PERMISSION_MEMBER = 2;
+	//管理员
+	public static final Integer PERMISSION_ADMIN = 3;
+}
+
