69014c41fe7cb13b768caca4765fe1459f9c06ad
zhujg <zhujg@asiainfo.com>
2016-11-23 16:12:59 +0800

    id=用户bean

---
+++ src/main/java/com/secondcar/bean/Role.java
@@ -0,0 +1,27 @@
+package com.secondcar.bean;
+
+public class Role {
+
+	private Integer id;
+	
+	private String roleName;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+	
+}
