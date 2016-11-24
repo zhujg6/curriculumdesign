a71851e016b32b1357ab6a595a76fce975584283
zhujg <zhujg@asiainfo.com>
2016-11-24 14:24:18 +0800

    id=log

--- src/main/java/com/secondcar/servicesImpl/UserServiceImpl.java
+++ src/main/java/com/secondcar/servicesImpl/UserServiceImpl.java
@@ -1,11 +1,20 @@
 package com.secondcar.servicesImpl;
 
+
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.secondcar.bean.User;
 import com.secondcar.comm.ResultInfo;
 import com.secondcar.services.UserService;
 
 public class UserServiceImpl implements UserService {
 
+	
+	private static Log logger = LogFactory.getLog(UserServiceImpl.class);
+	
 	public ResultInfo insertUser(User user) {
 		return null;
 	}
