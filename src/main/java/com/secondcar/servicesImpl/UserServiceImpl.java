2c7e489e0bd117a86006291ae20974e7543acbea
zhujg <zhujg@asiainfo.com>
2016-11-24 16:46:52 +0800

    Revert "Revert "id=邮箱""
    This reverts commit d309c8d68e630685f6f04c486cc95a06d0b4a279.

--- src/main/java/com/secondcar/servicesImpl/UserServiceImpl.java
+++ src/main/java/com/secondcar/servicesImpl/UserServiceImpl.java
@@ -10,6 +10,9 @@ import com.secondcar.bean.User;
 import com.secondcar.comm.BeanFactoryUtil;
 import com.secondcar.comm.ResultInfo;
 import com.secondcar.dao.UserDao;
+import com.secondcar.mail.InitMail;
+import com.secondcar.mail.Mail;
+import com.secondcar.mail.MailUtil;
 import com.secondcar.services.UserService;
 
 public class UserServiceImpl implements UserService {
@@ -24,7 +27,34 @@ public class UserServiceImpl implements UserService {
 	}
 
 	public ResultInfo login(User user) {
-		return null;
+		ResultInfo ret = new ResultInfo();
+		if(user != null){
+			if(user.getEmail()!=null && user.getEmail()!=""){
+				if(user.getPassword()!=null && user.getPassword()!=""){
+					User newuser =userDao.checkLogin(user);
+					if(newuser!=null){
+						ret.setCode(0);
+						ret.setObj(newuser);
+						Mail mail = InitMail.init(newuser.getEmail());
+						MailUtil.send(mail);
+					}else{
+						ret.setCode(-1);
+						ret.setInfo("µÇÂ¼Ê§°Ü£¡");
+					}
+				}else{
+					ret.setCode(-1);
+					ret.setInfo("µÇÂ¼ÃÜÂë²»ÄÜÎª¿Õ£¡");
+					logger.info("µÇÂ¼ÃÜÂë²»ÄÜÎª¿Õ£¡");
+				}
+			}else{
+				ret.setCode(-1);
+				ret.setInfo("µÇÂ¼ÓÊÏä²»ÄÜÎª¿Õ£¡");
+			}
+		}else{
+			ret.setCode(-1);
+			ret.setInfo("µÇÂ¼ÓÃ»§²»ÄÜÎª¿Õ£¡");
+		}
+		return ret;
 	}
 
 	public ResultInfo updateUserInfo(User user) {
@@ -34,5 +64,11 @@ public class UserServiceImpl implements UserService {
 	public ResultInfo queryUserInfo(String id) {
 		return null;
 	}
-
+	public static void main(String[] args) {
+		UserService userService = new UserServiceImpl();
+		User user = new User();
+		user.setEmail("915018368@qq.com");
+		user.setPassword("");
+		userService.login(user);
+	}
 }
